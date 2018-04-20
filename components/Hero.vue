1d7b4dda7a557ef403bd6378ff17386b30f28c4b
pratamawijaya <set.mnemonix@gmail.com>
2018-04-20 13:49:42 +0700

    add axios add more page for dota heroes

---
+++ components/Hero.vue
@@ -0,0 +1,34 @@
+<template>
+  <hero class="hero">
+      <div class="hero-img" :style="{backgroundImage: 'url('+ img +')'}"></div>
+      <h1>hero name</h1>
+  </hero>
+</template>
+
+<script>
+export default {
+  props: [
+      "id",
+      "name",
+      "img"
+  ]
+}
+</script>
+
+<style>
+    .hero {
+        box-sizing: border-box;
+        width: 200px;
+        padding: 8px;
+        border: 1px solid #ccc;
+        box-shadow: 0 2px 2px #aaa;
+        margin: 10px;
+    }
+
+    .thumbnail {
+        background-position: center;
+        background-size: cover;
+        width: 100%;
+        height: 200px;
+    }
+</style>
---
+++ pages/heroes/index.vue
@@ -0,0 +1,33 @@
+<template>
+   <ul v-if="heroes && heroes.lenth">
+      <li v-for="hero in heroes">
+          <p>{{hero.name}}</p>
+      </li>
+   </ul>
+</template>
+
+<script>
+import axios from 'axios';
+import Hero from '@/components/Hero';
+export default {
+  components : {
+      Hero
+  },
+  data () {
+      return {
+          heroes : [],
+          errors : []
+      }
+  },
+  created() {
+      axios.get('https://api.opendota.com/api/heroes')
+        .then(response => {
+            this.heroes = response.data
+            console.log(this.heroes)
+        })
+        .catch(e => {
+            this.errors.push(e)
+        })
+  }
+}
+</script>
